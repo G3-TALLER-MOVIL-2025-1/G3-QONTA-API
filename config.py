73f9cd7ac939bd2f23e1678c66c6e4c578d465a5
Sebastian Sanchez <jesus.sanchez15@unmsm.edu.pe>
2025-05-31 14:14:58 -0500

    Conf cadena a DB

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://qonta_user:kmobuqJMYLAVZIG1257DFRIyZmodXbSY@dpg-d0oulhmmcj7s73djs5m0-a.oregon-postgres.render.com/qonta")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://g3qontaadmin:1sg2eSWE7q5l7gZmWiCZTYX1Z0RWX1Kd@dpg-d0otab6uk2gs7392ifa0-a.virginia-postgres.render.com/qontadb")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "secret")
