4cfbb948126f4f8bc6be5ca857c548c4b0bea369
Sebastian Sanchez <jesus.sanchez15@unmsm.edu.pe>
2025-05-31 14:56:53 -0500

    cadenaBD2

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine, Column
 from sqlalchemy.ext.declarative import declarative_base
 
 # db = SQLAlchemy()
-engine = create_engine("postgresql://dpg-d0otab6uk2gs7392ifa0-a.virginia-postgres.render.com:5432/qontadb")
+engine = create_engine("postgresql://g3qontaadmin:1sg2eSWE7q5l7gZmWiCZTYX1Z0RWX1Kd@dpg-d0otab6uk2gs7392ifa0-a.virginia-postgres.render.com/qontadb")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # db = SessionLocal()
 Base = declarative_base()
